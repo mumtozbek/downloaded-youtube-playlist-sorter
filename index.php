2586e3c26a7c61534103010911b61c8ba9f7f141
Mumtoz Kodirov <7757300+mumtozbek@users.noreply.github.com>
2020-09-09 20:54:13 +0500

    Create function mb_ucfirst, if mb extension is enabled

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 // Definitions
 require_once ('config.php');
 
+// Create function mb_ucfirst, if mb extension is enabled
+if (!function_exists('mb_ucfirst')) {
+  function mb_ucfirst($string, $enc = 'UTF-8') {
+    return mb_strtoupper(mb_substr($string, 0, 1, $enc), $enc) . mb_substr($string, 1, mb_strlen($string, $enc), $enc);
+  }
+}
+
 // Functions
 function getMetaFiles($path) {
   $items = [];
